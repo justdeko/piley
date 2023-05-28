467fc5a25d34157c05a6aa12e1a84f9b5788e18b
Denis Koljada <denis.koljada@campus.tu-berlin.de>
2023-05-28 16:27:28 +0200

    PileTitlePager swipe animations

--- app/src/main/java/com/dk/piley/ui/pile/PileTitlePager.kt
+++ app/src/main/java/com/dk/piley/ui/pile/PileTitlePager.kt
@@ -1,5 +1,15 @@
 package com.dk.piley.ui.pile
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dk.piley.ui.theme.PileyTheme
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun PileTitlePager(
     modifier: Modifier = Modifier,
@@ -42,14 +53,23 @@ fun PileTitlePager(
                 contentDescription = "switch to left pile"
             )
         }
-        Text(
-            text = pileTitleList.getOrElse(selectedPageIndex) { "" },
-            modifier = Modifier
-                .weight(1f),
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-            textAlign = TextAlign.Center
-        )
+        AnimatedContent(
+            modifier = Modifier.weight(1f),
+            targetState = selectedPageIndex,
+            transitionSpec = {
+                titleSlideAnimation(leftToRight = targetState > initialState).using(
+                    SizeTransform(clip = false)
+                )
+            }
+        ) { index ->
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = pileTitleList.getOrElse(index) { "" },
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onBackground,
+                textAlign = TextAlign.Center
+            )
+        }
         IconButton(
             onClick = { onPageChanged(selectedPageIndex + 1) },
             enabled = selectedPageIndex != pileTitleList.lastIndex
@@ -62,6 +82,15 @@ fun PileTitlePager(
         }
     }
 }
+
+@ExperimentalAnimationApi
+fun titleSlideAnimation(duration: Int = 500, leftToRight: Boolean = true): ContentTransform =
+    slideInHorizontally(animationSpec = tween(durationMillis = duration)) { width -> if (leftToRight) width else -width } + fadeIn(
+        animationSpec = tween(durationMillis = duration)
+    ) with slideOutHorizontally(animationSpec = tween(durationMillis = duration)) { width -> if (leftToRight) -width else width } + fadeOut(
+        animationSpec = tween(durationMillis = duration)
+    )
+
 
 @Preview(showBackground = false)
 @Composable

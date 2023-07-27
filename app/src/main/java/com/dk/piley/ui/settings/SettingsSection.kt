26097c7ff111c5f4fd61cf1722ebd24d506145c4
Denis Koljada <denis.koljada@gmail.com>
2023-07-27 22:37:20 +0200

    add expandable content items, make settings expandable

--- app/src/main/java/com/dk/piley/ui/settings/SettingsSection.kt
+++ app/src/main/java/com/dk/piley/ui/settings/SettingsSection.kt
@@ -1,6 +1,12 @@
 package com.dk.piley.ui.settings
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Divider
@@ -18,34 +24,45 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dk.piley.ui.common.ExpandableContent
 import com.dk.piley.ui.theme.PileyTheme
 
 @Composable
 fun SettingsSection(
     modifier: Modifier = Modifier, title: String, icon: ImageVector, items: @Composable () -> Unit
 ) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
     Column(modifier = modifier.fillMaxWidth()) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Icon(
-                icon,
-                modifier = Modifier.scale(1.3F),
-                tint = MaterialTheme.colorScheme.secondary,
-                contentDescription = null,
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = title,
-                color = MaterialTheme.colorScheme.primary,
-                style = MaterialTheme.typography.headlineSmall,
-            )
+        ExpandableContent(
+            onHeaderClick = { expanded = !expanded },
+            onArrowClick = { expanded = !expanded },
+            expanded = expanded,
+            headerContent = {
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        icon,
+                        modifier = Modifier.scale(1.3F),
+                        tint = MaterialTheme.colorScheme.secondary,
+                        contentDescription = null,
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = title,
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.headlineSmall,
+                    )
+                }
+            }) {
+            Column(modifier = modifier.fillMaxWidth()) {
+                items()
+            }
         }
-        items()
         Divider(modifier = Modifier.padding(vertical = 4.dp))
     }
 }

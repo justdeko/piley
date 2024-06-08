50ff3418a17158c745d523c564666bbc79bc61ca
Denis Koljada <denis@koljada.com>
2024-06-08 20:09:35 +0200

    prepare for 0.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dk.piley"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.6.2"
+        versionCode = 10
+        versionName = "0.6.5"
 
         // default api base urls
         val url = project.findProperty("API_URL") as String? ?: "https://127.0.0.1/"

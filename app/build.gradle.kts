e351a96cfd593b3eca4fa69c77386cce87a7e159
Denis Koljada <denis@koljada.com>
2024-06-08 22:49:53 +0200

    hotfix for turkish translation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dk.piley"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.6.5"
+        versionCode = 11
+        versionName = "0.6.6"
 
         // default api base urls
         val url = project.findProperty("API_URL") as String? ?: "https://127.0.0.1/"

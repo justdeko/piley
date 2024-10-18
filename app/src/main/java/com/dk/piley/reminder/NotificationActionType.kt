19b7597a170ce33d86a944e6735ee71efb02ff85
Denis Koljada <denis@koljada.com>
2024-10-18 20:43:26 +0200

    delete app directory

--- app/src/main/java/com/dk/piley/reminder/NotificationActionType.kt
+++
@@ -1,5 +0,0 @@
-package com.dk.piley.reminder
-
-enum class NotificationActionType {
-    Done, Delay, CustomDelay
-}
--- app/src/main/java/com/dk/piley/ui/signin/TestUser.kt
+++
@@ -1,9 +0,0 @@
-package com.dk.piley.ui.signin
-
-import com.dk.piley.model.remote.user.UserRequest
-
-val testUser = UserRequest(
-    email = "test.user@pileyapp.com",
-    password = "123456",
-    name = "test user"
-)

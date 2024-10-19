f34d52173bfe4bfef037176821acf8a578c426f7
Denis Koljada <denis@koljada.com>
2024-10-19 17:16:20 +0200

    ios and desktop apps

---
+++ composeApp/src/commonMain/kotlin/com/dk/piley/MainContent.kt
@@ -0,0 +1,10 @@
+package com.dk.piley
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainContent() {
+    // TODO: fix for projcet.pbxproj still needed
+    Text("hi there")
+}

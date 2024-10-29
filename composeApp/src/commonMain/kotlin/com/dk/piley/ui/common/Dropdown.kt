e30bc7fa461e3e864228b1f30ff7632929ccc579
Denis Koljada <denis@koljada.com>
2024-10-29 13:46:59 +0100

    fix desktop issues

--- composeApp/src/commonMain/kotlin/com/dk/piley/ui/common/Dropdown.kt
+++ composeApp/src/commonMain/kotlin/com/dk/piley/ui/common/Dropdown.kt
@@ -6,12 +6,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalTextInputService
 
 /**
  * Generic drop down element
@@ -44,23 +43,18 @@ fun DropDown(
         expanded = expanded,
         onExpandedChange = onExpandedChange,
     ) {
-        // disable text input provider due to bug where keyboard pops up despite readOnly = true
-        CompositionLocalProvider(
-            LocalTextInputService provides null
-        ) {
-            OutlinedTextField(
-                modifier = Modifier
-                    .menuAnchor()
-                    .fillMaxWidth(),
-                readOnly = true,
-                value = value,
-                shape = MaterialTheme.shapes.large,
-                onValueChange = {},
-                label = label?.let { { Text(label) } },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
-            )
-        }
+        OutlinedTextField(
+            modifier = Modifier
+                .menuAnchor(MenuAnchorType.PrimaryNotEditable)
+                .fillMaxWidth(),
+            readOnly = true,
+            value = value,
+            shape = MaterialTheme.shapes.large,
+            onValueChange = {},
+            label = label?.let { { Text(label) } },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+        )
         ExposedDropdownMenu(
             modifier = Modifier.exposedDropdownSize(),
             expanded = expanded,
--- composeApp/src/desktopMain/kotlin/com/dk/piley/util/PlatformUtil.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/dk/piley/util/PlatformUtil.desktop.kt
@@ -1,3 +1,14 @@
 package com.dk.piley.util
 
-actual fun getVersionName(): String = System.getProperty("jpackage.app-version")
+// TODO fix this https://stackoverflow.com/a/73121886/4464680
+actual fun getVersionName(): String {
+    return try {
+        val x = System.getProperty("jpackage.app-version")
+        println(x)
+        if (x == "null" || x == null) VERSION_NUMBER else x
+    } catch (e: Exception) {
+        VERSION_NUMBER
+    }
+}
+
+const val VERSION_NUMBER = "0.7.0"

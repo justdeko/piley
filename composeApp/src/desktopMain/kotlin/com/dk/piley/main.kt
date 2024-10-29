e69fe5921f3dd5cf9713bd8f65887bf11b833893
Denis Koljada <denis@koljada.com>
2024-10-29 19:13:15 +0100

    fix some iOS issues

--- composeApp/src/desktopMain/kotlin/com/dk/piley/main.kt
+++ composeApp/src/desktopMain/kotlin/com/dk/piley/main.kt
@@ -4,6 +4,9 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.dk.piley.ui.HomeScreen
 import com.dk.piley.ui.theme.ThemeHostScreen
+import org.jetbrains.compose.resources.painterResource
+import piley.composeapp.generated.resources.Res
+import piley.composeapp.generated.resources.icon_transparent
 
 fun main() = application {
     // di init
@@ -12,6 +15,8 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "piley",
+        // todo use also distribution icon https://github.com/JetBrains/compose-multiplatform/tree/master/tutorials/Native_distributions_and_local_execution#app-icon
+        icon = painterResource(Res.drawable.icon_transparent)
     ) {
         ThemeHostScreen {
             HomeScreen()

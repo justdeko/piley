a7d39628c7da28b1ad6512ed776c0e80f0c0de22
Denis Koljada <denis@koljada.com>
2024-12-04 18:46:35 +0100

    upgrade to sdk 35

--- composeApp/src/commonMain/kotlin/com/dk/piley/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dk/piley/ui/HomeScreen.kt
@@ -99,6 +99,7 @@ fun HomeScreen(
         modifier = modifier.then(
             // safe padding on all screens except splash screen
             if (navBackStackEntry?.destination?.route == Screen.Splash.route) Modifier
+            // TODO adapt for sdk 35
             else Modifier.windowInsetsPadding(WindowInsets.statusBars) // TODO fix this for ios with box and surfaceContainer color on the lower bottom half
         ),
         containerColor = MaterialTheme.colorScheme.background,

1740089747c6726bac037df0da55423fc4a6f6a2
Denis Koljada <denis@koljada.com>
2025-04-08 19:56:14 +0200

    fix keyboard background on iOS

--- composeApp/src/iosMain/kotlin/com/dk/piley/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/dk/piley/MainViewController.kt
@@ -1,10 +1,15 @@
 package com.dk.piley
 
+import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.ui.window.ComposeUIViewController
 import com.dk.piley.ui.HomeScreen
 import com.dk.piley.ui.theme.ThemeHostScreen
 
-fun MainViewController() = ComposeUIViewController {
+@OptIn(ExperimentalComposeApi::class)
+fun MainViewController() = ComposeUIViewController(configure = {
+    // TODO: check if this can be removed: https://youtrack.jetbrains.com/issue/CMP-7943
+    opaque = false
+}) {
     ThemeHostScreen {
         HomeScreen()
     }

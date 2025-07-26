d7c6e6779fa4df42f1a9f52c980c8c4482d88329
Denis Koljada <denis@koljada.com>
2025-07-26 16:32:34 +0200

    increase version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -90,7 +90,7 @@ android {
         applicationId = "com.dk.piley"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
+        versionCode = 23
         versionName = "0.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
